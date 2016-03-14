697a054df7c31600029dfd5142693bce62ed7909
JamesMarkWilton <james.mark.wilton@gmail.com>
2016-03-14 11:30:42 -0500

    Did some small edits

--- spec/module_challenges/challenges.rb
+++ spec/module_challenges/challenges.rb
@@ -2,13 +2,32 @@ require 'spec_helper'
 require 'module_challenges'
 
 module MahMod
+  def self.find_c(mod)
+    mod::C
+  end
+
+  module String
+  end
+
+  Dir = "These aren't the Dirs you're looking for"
+
+  def self.mah_dir
+    Dir
+  end
+
+  def self.top_dir
+    ::Dir
+  end
+
+  def self.make_mod(mod_name)
+
+  end
+
   module A
-    module C
-      1
-    end
+    C = 1
   end
 
   module B
-
+    C = 2
   end
 end
